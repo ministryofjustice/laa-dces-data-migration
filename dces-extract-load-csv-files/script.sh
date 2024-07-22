0af34c0441a405f5ee0cc30392e36602b9cf98e5
tariqhossain <tariq.hossain@digital.justice.gov.uk>
2024-07-22 20:16:22 +0100

    handles json footer correctly

--- dces-extract-load-csv-files/script.sh
+++ dces-extract-load-csv-files/script.sh
@@ -6,15 +6,6 @@ if [ -z "$PGUSER" ]; then
     exit 1
 fi
 
-
-
-# Prompt for batch ID
-# read -p "Enter batch ID (leave blank to use current date and time): " BATCH_ID
-
-# Use BATCH_ID environment variable if provided, otherwise default to current date and time
-# BATCH_ID="${BATCH_ID:-$(date +"%d%m%y%H%M")}"
-
-
 # Check if BATCH_ID is set, if not, print an error message and exit
 if [ -z "$BATCH_ID" ]; then
     echo "Error: BATCH_ID is not set"
@@ -22,10 +13,6 @@ if [ -z "$BATCH_ID" ]; then
 fi
 
 echo BATCH_ID=$BATCH_ID
-
-
-#S3_PREFIX="${S3_PREFIX:-DRC/data/}"
-
 
 # Check if S3_PREFIX is set, if not, print an error message and exit
 if [ -z "$S3_PREFIX" ]; then
@@ -39,10 +26,7 @@ if [[ "${S3_PREFIX: -1}" != "/" ]]; then
     exit 1
 fi
 
-
-
 echo S3_PREFIX=$S3_PREFIX
-
 
 # Check if FILE_PATTERN is set, if not, print an error message and exit
 if [ -z "$FILE_PATTERN" ]; then
@@ -50,12 +34,7 @@ if [ -z "$FILE_PATTERN" ]; then
     exit 1
 fi
 
-
 echo FILE_PATTERN=$FILE_PATTERN
-
-
-
-
 
 # Define variables
 CSV_DIR="/app/data/"
@@ -90,70 +69,68 @@ remove_carriage_returns() {
 # Loop through CSV files in the directory
 for CSV_FILE in "$CSV_DIR"$FILE_PATTERN*.csv; do
 
-# Strip BOM from the CSV file
-  strip_bom "$CSV_FILE"
+    # Strip BOM from the CSV file
+    strip_bom "$CSV_FILE"
 
-# Remove carriage return characters
-  remove_carriage_returns "$CSV_FILE"
+    # Remove carriage return characters
+    remove_carriage_returns "$CSV_FILE"
 
-  # Extract base table name from CSV file name
-  BASE_TABLE_NAME=$(basename "$CSV_FILE" | sed 's/^file1_//' | sed 's/\.csv$//')
+    # Extract base table name from CSV file name
+    BASE_TABLE_NAME=$(basename "$CSV_FILE" | sed 's/^file1_//' | sed 's/\.csv$//')
 
-  # Incorporate batch ID into the table name
-  TABLE_NAME="marston.${BASE_TABLE_NAME}_${BATCH_ID}"
+    # Incorporate batch ID into the table name
+    TABLE_NAME="marston.${BASE_TABLE_NAME}_${BATCH_ID}"
 
-  echo $TABLE_NAME
+    echo $TABLE_NAME
 
-  # Extract header from CSV file
-  HEADER=$(head -n 1 "$CSV_FILE")
+    # Extract header from CSV file
+    HEADER=$(head -n 1 "$CSV_FILE")
 
-  # Convert header to lowercase and replace spaces with underscores
-  HEADER=$(echo "$HEADER" | tr '[:upper:]' '[:lower:]' | sed 's/ /_/g')
+    # Convert header to lowercase and replace spaces with underscores
+    HEADER=$(echo "$HEADER" | tr '[:upper:]' '[:lower:]' | sed 's/ /_/g')
 
+    # Add batch_id column to the beginning of the header and json_footer column at the end
+    HEADER="batch_id,${HEADER},json_footer"
 
-  # Add batch_id column to the beginning of the header and json_footer column at the end
-  HEADER="batch_id,${HEADER},json_footer"
+    # Create SQL column definitions
+    COLUMNS=$(echo "$HEADER" | awk -F, '{ for(i=1; i<=NF; i++) print $i (i==NF ? " jsonb," : " text,") }' | sed '$ s/,$//')
 
-  # Create SQL column definitions
-  COLUMNS=$(echo "$HEADER" | awk -F, '{ for(i=1; i<=NF; i++) print $i " text," }' | sed '$ s/,$//')
+    # Create SQL statements
+    DROP_TABLE_SQL="drop table if exists $TABLE_NAME;\n"
+    CREATE_TABLE_SQL="create table $TABLE_NAME (\n$COLUMNS\n);\n"
 
+    # Create a temporary CSV file with the batch ID column in the to-be-processed directory
+    TMP_CSV_FILE="$TO_BE_PROCESSED_DIR${BASE_TABLE_NAME}_${BATCH_ID}_with_batch_id.csv"
+    {
+        echo "$HEADER"
+        awk -v batch_id="$BATCH_ID" -F, 'NR > 1 { print batch_id "," $0 "," }' "$CSV_FILE" | head -n -1
+    } > "$TMP_CSV_FILE"
 
-  # Create SQL statements
-  DROP_TABLE_SQL="drop table if exists $TABLE_NAME;\n"
-  CREATE_TABLE_SQL="create table $TABLE_NAME (\n$COLUMNS\n);\n"
-
-  # Create a temporary CSV file with the batch ID column in the to-be-processed directory
-  TMP_CSV_FILE="$TO_BE_PROCESSED_DIR${BASE_TABLE_NAME}_${BATCH_ID}_with_batch_id.csv"
-  {
-    echo "$HEADER"
-    #awk -v batch_id="$BATCH_ID" -F, 'NR > 1 { print batch_id "," $0 }' "$CSV_FILE"
-    awk -v batch_id="$BATCH_ID" -F, 'NR > 1 { print batch_id "," $0 ",\"\""}' "$CSV_FILE" | head -n -1
-  } > "$TMP_CSV_FILE"
-
-# Extract the footer
+    # Extract the footer
     FOOTER=$(tail -n 1 "$CSV_FILE")
-    FOOTER_JSON=$(echo "$FOOTER" | awk -F, '{printf "{\"createdate\":\"%s\",\"weeknum\":\"%s\",\"daynum\":\"%s\",\"rowcount\":\"%s\",\"deltastartdate\":\"%s\",\"deltaenddate\":\"%s\"}", $1, $2, $3, $4, $5, $6}')
+    FOOTER_JSON=$(echo "$FOOTER" | awk -F, '{printf "{ \"createdate\": \"%s\", \"weeknum\": \"%s\", \"daynum\": \"%s\", \"rowcount\": \"%s\", \"deltastartdate\": \"%s\", \"deltaenddate\": \"%s\" }", $1, $2, $3, $4, $5, $6}')
 
-# Get the number of columns (including the new ones for batch_id and json_footer)
+    # Properly escape the double quotes for the JSON string
+    FOOTER_JSON_ESCAPED=$(echo "$FOOTER_JSON" | sed 's/"/""/g')
+
+    # Get the number of columns (including the new ones for batch_id and json_footer)
     NUM_COLUMNS=$(echo "$HEADER" | awk -F, '{print NF}')
 
     # Create an empty row with just the footer JSON in the last column
-    EMPTY_COLUMNS=$(for i in $(seq 1 $((NUM_COLUMNS-1))); do printf ","; done)
-    echo "$BATCH_ID$EMPTY_COLUMNS\"$FOOTER_JSON\"" >> "$TMP_CSV_FILE"
+    EMPTY_COLUMNS=$(for i in $(seq 1 $((NUM_COLUMNS-2))); do printf ","; done)
+    echo "$BATCH_ID$EMPTY_COLUMNS,\"$FOOTER_JSON_ESCAPED\"" >> "$TMP_CSV_FILE"
 
+    # Create the COPY SQL statement
+    COPY_SQL="copy $TABLE_NAME from '$TMP_CSV_FILE' delimiter ',' CSV HEADER;\n"
+    BS='\'
 
-
-  # Create the COPY SQL statement
-  COPY_SQL="copy $TABLE_NAME from '$TMP_CSV_FILE' delimiter ',' CSV HEADER;\n"
-  BS='\'
-
-  # Write SQL statements to the SQL file
-  {
-    echo -e "$DROP_TABLE_SQL"
-    echo -e "$CREATE_TABLE_SQL"
-    echo -n $BS
-    echo -e $COPY_SQL
-  } >> "$SQL_FILE"
+    # Write SQL statements to the SQL file
+    {
+        echo -e "$DROP_TABLE_SQL"
+        echo -e "$CREATE_TABLE_SQL"
+        echo -n $BS
+        echo -e $COPY_SQL
+    } >> "$SQL_FILE"
 
 done
 
@@ -166,7 +143,7 @@ psql -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d "$PGDATABASE" -W -f "$SQL_FILE"
 # Check if the psql command was successful
 if [ $? -eq 0 ]; then
   # Move processed files to the S3 processed directory
-  for CSV_FILE in "$CSV_DIR"$FILE_PATERN*.csv; do
+  for CSV_FILE in "$CSV_DIR"$FILE_PATTERN*.csv; do
     BASE_FILE_NAME=$(basename "$CSV_FILE")
  #   aws s3 cp s3://$S3_BUCKET/$S3_PREFIX"$BASE_FILE_NAME" s3://$S3_BUCKET/LAA/$S3_PREFIX"processed/" --profile=dces-admin-user-dev
  #   aws s3api delete-object --bucket $S3_BUCKET --key LAA/data/"$BASE_FILE_NAME" --profile=dces-admin-user-dev
