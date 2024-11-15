98e9537d79627bbdfa5659b9fe70e84e3f21e077
abecetin <abe.cetin@digital.justice.gov.uk>
2024-11-15 09:51:51 +0000

    Update transform_filter_migrationscope.sql
    StatusCodeDate criteria added as per DCES-599

--- dev_db/marston/routines/transform_filter_migrationscope.sql
+++ dev_db/marston/routines/transform_filter_migrationscope.sql
@@ -1,9 +1,9 @@
 -- This script checks if the `MigrationScope` table exists in the `marston` schema. If it exists, the table is truncated; if not, it creates the table.
 -- The script then populates the `MigrationScope` table with data from multiple sources.
 -- It aggregates maximum loaded dates from `laacasenotes_20241018` and `laacasehistory_20241018`, and selects relevant `caseid`, `maatid`, and `lacescaseid` values from `laacasedetails_20241018`.
--- Records are filtered based on whether the case is open or if the latest activity date in case notes or history is later than January 31, 2023.
--- Additional criteria added to filter clientcasereference.
--- https://dsdmoj.atlassian.net/browse/DCES-564
+-- Records are filtered based on whether the case is open or if the latest activity date in case notes, history, or statusdate is later than January 31, 2023.
+-- Additional criteria added to filter clientcasereference https://dsdmoj.atlassian.net/browse/DCES-564
+-- 151124 - AC: statuscodedate added as per DCES-599
 
 DO $$
 BEGIN
@@ -48,7 +48,8 @@ BEGIN
     WHERE (a.openclosedstatus = 'OPEN'
         OR GREATEST(
             COALESCE(mn.max_loadedon_casenotes, '1900-01-01'::timestamp),
-            COALESCE(mh.max_loadedon_casehistory, '1900-01-01'::timestamp)
+            COALESCE(mh.max_loadedon_casehistory, '1900-01-01'::timestamp),
+            COALESCE(a.statusdate::timestamp, '1900-01-01'::timestamp)  -- 151124: Added for DCES-599
         ) > '2023-01-31')
       -- 06112024: Criteria below added for DCES-563  
       AND (NOT (a.clientcasereference !~ '^\d{7}$')
